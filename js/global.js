84a46520ae065c927842292eb6933ae74cfb4336
satvik <satvikshukla453@gmail.com>
2024-08-22 23:02:03 +0530

    basic skill

--- js/global.js
+++
@@ -1,3 +0,0 @@
-const script = document.createElement("script");
-script.src = "https://cdn.tailwindcss.com"
-document.getElementsByTagName("head")[0].append(script)
--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,19 @@
 const navNode = document.createElement("div");
-console.log("OK NAV")
+
+const isHome = location.pathname.includes("index.html")
+const isSkill = location.pathname.includes("skill.html")
+
 navNode.innerHTML = `
-    <div id="navbar" class="w-full bg-red-200 justify-between px-5 py-2">
-        <img src="../public/Logo.png" class="" />
+    <div id="navbar" class="w-full justify-between px-10 py-2 flex items-center fixed left-0 top-0 bg-white z-10 shadow-md">
+        <img src="public/Logo.png" class="" />
+        <div class="flex gap-4">
+            <a href="index.html" class="px-4 py-1 rounded-md ${isHome && "bg-amber-400"}">Home</a>
+            <a href="skill.html" class="px-4 py-1 rounded-md ${isSkill && "bg-amber-400"}">Skills</a>
+            <a href="#" class="px-4 py-1 rounded-md">Education</a>
+            <a href="#" class="px-4 py-1 rounded-md">Projects</a>
+            <a href="#" class="px-4 py-1 rounded-md">Recommendations</a>
+            <a href="#" class="px-4 py-1 rounded-md">Contact</a>
+        </div>
     </div>
     `
 body.prepend(navNode);
